0817dad3a3e8f3fada0e8f87435c2d53c63e3619
Fanming Kong <kongfanming@lixiang.com>
2023-04-26 15:10:57 +0800

    use script to get android root dir
    Signed-off-by: Fanming Kong <kongfanming@lixiang.com>

--- gen_compdb_kernel.sh
+++ gen_compdb_kernel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 product='HU_SS3'
+android_root_dir=$(dirname $(dirname $(dirname "$(cd "$(dirname "$0")";pwd)")))
 python3 generate_compdb_kernel.py --target $product \
-    --android_root /home/fanming/data/work/projects/ss3_8295/code/lagvm/LINUX/android \
-    --docker_android_root /home/fanming/workspace/lagvm/LINUX/android/
+    --android_root $android_root_dir \
+    --docker_android_root $HOME/workspace/lagvm/LINUX/android/
